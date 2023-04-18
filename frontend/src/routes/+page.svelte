487c4f36a50d20bcf76e93a1e0a68e05dac56200
takenokoroid <takenokoroid@gmail.com>
2023-04-18 15:10:02 +0000

    #5 index.htmlの内容をsvelteに取り込んだ

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,55 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	let message = 'WebSocket未接続';
+	let audioSrc = '';
+	let socket;
+
+	onMount(() => {
+		// WebSocket URL
+		socket = new WebSocket('wss://????????.execute-api.ap-northeast-1.amazonaws.com/production');
+		// WebSocketのエラー処理
+		socket.onerror = (e) => {
+			console.error(e);
+		};
+
+		// 接続後、表示する文字列を変更する
+		socket.onopen = () => {
+			message = 'WebSocket接続中';
+		};
+
+		// 画像保存先のS3 URLが送られてくるので、imgタグのsrcをそのURLに変更する
+		socket.onmessage = (e) => {
+			audioSrc = e.data;
+		};
+	});
+
+	const formSend = () => {
+		socket.send(
+			JSON.stringify({
+				action: 'send_voice',
+				selectedImage: 'zn_sugoinoda'
+			})
+		);
+	};
+</script>
+
+<audio autoplay src={audioSrc} />
+<button on:click={() => formSend()}>すごいのだ</button>
+
+<div id="box" class="box">
+	<p>{message}</p>
+</div>
+
+<style>
+	.box {
+		display: inline-block;
+		border: double;
+	}
+
+	.box p {
+		margin: 1px;
+		padding: 1px;
+		font-size: 80%;
+	}
+</style>
